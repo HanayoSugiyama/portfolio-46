0507eef1037e52104db668933feda975bc1389ee
HanayoSugiyama <148828272+HanayoSugiyama@users.noreply.github.com>
2023-12-05 21:59:24 +0900

    874

--- js/script.js
+++ js/script.js
@@ -1,30 +1,127 @@
-
 const itemFrame = document.querySelector('.portfolio__item__list');
 const itemList = [{
-  name: '08 杉山花代',
+  name: '03 江藤',
   img: 'img/',
-  url:'https://hanayosugiyama.github.io/portfolio874/',
- 
-},  {
-    name: '08 杉山花代',
-    img: 'img/',
-    url:'',
-   
-  }, {
-    name: '08 杉山花代',
-    img: 'img/',
-    url:'',
-   
-  }];
+  url: '',
+}, {
+  name: '04 奥田',
+  img: 'img/',
+  url: '',
+
+}, {
+  name: '05 梶原',
+  img: 'img/',
+  url: '',
+
+}, {
+  name: '06 木根森',
+  img: 'img/',
+  url: '',
+
+}, {
+  name: '07 末永',
+  img: 'img/',
+  url: '',
+
+}, {
+  name: '08 杉山',
+  img: 'img/',
+  url: '',
+
+}, {
+  name: '09 園田',
+  img: 'img/',
+  url: '',
+
+}, {
+  name: '10 高橋',
+  img: 'img/',
+  url: '',
+
+}, {
+  name: '11 ',
+  img: 'img/',
+  url: '',
+
+}, {
+  name: '12',
+  img: 'img/',
+  url: '',
+
+}, {
+  name: '13',
+  img: 'img/',
+  url: '',
+
+}, {
+  name: '14',
+  img: 'img/',
+  url: '',
+
+}, {
+  name: '15',
+  img: 'img/',
+  url: '',
+
+}, {
+  name: '16',
+  img: 'img/',
+  url: '',
+
+}, {
+  name: '17',
+  img: 'img/',
+  url: '',
+
+}, {
+  name: '18',
+  img: 'img/',
+  url: '',
+
+}, {
+  name: '19',
+  img: 'img/',
+  url: '',
+
+}, {
+  name: '20 ',
+  img: 'img/',
+  url: '',
+
+}, {
+  name: '21',
+  img: 'img/',
+  url: '',
+
+}, {
+  name: '22',
+  img: 'img/',
+  url: '',
+
+}, {
+  name: '23',
+  img: 'img/',
+  url: '',
+
+}, {
+  name: '25',
+  img: 'img/',
+  url: '',
+
+},{
+  name: '26',
+  img: 'img/',
+  url: '',
+}];
 
 
-  itemList.forEach((i) => {
-    const {
-      name,
-      img,
-      url,
-    } = i;
-    const itemContent = `<li><a href="${url}" class="gallery" data-group="gallery"><div class="portfolio__item__img"><img src="${img}" alt="${name}"></div>
-  <div class="portfolio__item__text"><b>${name}ポートフォリオ</b></div</a></li>`;
-    itemFrame.insertAdjacentHTML('beforeend', itemContent);
-  })
+itemList.forEach((i) => {
+  const {
+    name,
+    img,
+    url,
+  } = i;
+  const itemContent = `<li><a href="${url}" class="gallery" data-group="gallery"><div class="portfolio__item__img"><img src="${img}" alt="${name}"></div>
+  <div class="portfolio__item__text"><p>${name}</p></div></a></li>`;
+  itemFrame.insertAdjacentHTML('beforeend', itemContent);
+})
